29b41c6e826100382cb244e764966f311cf47fc2
khinemyaezin <khinemyaezin@icloud.com>
2026-06-21 23:52:56 +0700

    chore: rename project directory path in CI workflow to grab-seller-inventory

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     federation({
-      name: "seller_inventory",
+      name: "grab_seller_inventory",
       filename: "remoteEntry.js",
       manifest: true,
       dts: { generateTypes: { tsConfigPath: "./tsconfig.app.json", abortOnError: true } },
